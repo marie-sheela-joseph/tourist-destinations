96d4b6897beeb0e58a6feb3d1d175e0a446a87c3
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-03-21 22:51:55 +0100

    Display tourist destinations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Display from './Display';
+
+const initialState=[{id:1,name:'Paris',country:'France'},
+  {id:2,name:'London',country:'United Kingdom'},
+  {id:3,name:'Rome',country:'Italy'},
+  ]
+
+interface ITouristDestinations {
+  touristDestinations:{  id: number;
+    name: string;
+    country: string;}[]    
+  };
 
 function App() {
+  const [touristDestinations,setTouristDestinations]=useState<ITouristDestinations["touristDestinations"]>(initialState);
   return (
-    <div>
-      <h1>React</h1>
+    <div>      
+      <Display touristDestinations={touristDestinations}/>
     </div>
   );
 }
-
 export default App;
