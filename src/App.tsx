2d0a387f7616159da6584e2534a85b500df39c20
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-03-22 10:26:20 +0100

    edit tourist destinations

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Add from './Add';
 import './App.css';
 import Display from './Display';
+import Edit from './Edit';
 
 const initialState=[{id:1,name:'Paris',country:'France'},
   {id:2,name:'London',country:'United Kingdom'},
@@ -9,23 +10,38 @@ const initialState=[{id:1,name:'Paris',country:'France'},
   ]
 
 interface ITouristDestinations {
-  touristDestinations:{  id: number;
-    name: string;
-    country: string;}[]    
+  touristDestinations:{  id: number,
+    name: string,
+    country: string}[]    
   };
+  export interface ITouristDestination{
+    id: number,
+    name: string,
+    country: string
+  }
 
 function App() {
   const [touristDestinations,setTouristDestinations]=useState<ITouristDestinations["touristDestinations"]>(initialState);
+  const [showEdit,setShowEdit]=useState(false);
+  const [editId,setEditId]=useState(0);
   function deleteTouristDestination(id:number){
-    setTouristDestinations((prevState)=>prevState.filter((t)=>t.id!==id))
+    setTouristDestinations((prevState)=>prevState.filter((t)=>t.id!==id));
   }
   function addTouristDestination(newTouristDestination:{name:string,country:string}):void{    
-    setTouristDestinations((prevState)=>{return [...prevState,{...newTouristDestination, id:prevState.length+1}]})
+    setTouristDestinations((prevState)=>{return [...prevState,{...newTouristDestination, id:prevState.length+1}]});
+  } 
+  function editTouristDestination(newTouristDestination:ITouristDestination){
+    setTouristDestinations(touristDestinations.map((t)=>t.id===editId?newTouristDestination:t))
+    setShowEdit(false)
+  }
+  function findTouristDestination():ITouristDestination{
+    return touristDestinations.find((t)=>t.id===editId)!
   }
   return (
     <div>      
       <Add addTouristDestination={addTouristDestination}/>
-      <Display touristDestinations={touristDestinations} deleteTouristDestination={deleteTouristDestination}/>
+      {showEdit && <Edit touristDestination={findTouristDestination()} editTouristDestination={editTouristDestination}/>}
+      <Display touristDestinations={touristDestinations} deleteTouristDestination={deleteTouristDestination} setShowEdit={setShowEdit} setEditId={setEditId}/>
     </div>
   );
 }
--- src/Display.tsx
+++ src/Display.tsx
@@ -2,17 +2,22 @@ interface IDisplay {
     touristDestinations:{  id: number;
       name: string;
       country: string;}[],
-      deleteTouristDestination:(id:number)=>void
+      deleteTouristDestination:(id:number)=>void,
+      setShowEdit:(s:boolean)=>void,
+      setEditId:(id:number)=>void
     };
 
-const Display:React.FC<IDisplay>=({touristDestinations,deleteTouristDestination}):JSX.Element=>{
-       
+const Display:React.FC<IDisplay>=({touristDestinations,deleteTouristDestination,setShowEdit,setEditId}):JSX.Element=>{       
     return (
         <div>
             <ol>
-                {touristDestinations.map((t)=><li>
+                {touristDestinations.map((t)=><li key={t.id}>
                     {t.name} in {t.country}
                     <button onClick={()=>deleteTouristDestination(t.id)}>Delete</button>
+                    <button onClick={()=>{
+                        setEditId(t.id)
+                        setShowEdit(true)                        
+                        }}>Edit</button>
                     </li>)}
             </ol>
         </div>
